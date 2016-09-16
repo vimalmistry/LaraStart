ff3e719cb48f001eecdaea1cc6e1d97e82f86098
Vimal Mistry <vimalmistry10@gmail.com>
2016-09-16 17:05:02 +0530

    Minor changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading"><i class="fa fa-home fa-fw"></i> Dashboard</div>
 
                 <div class="panel-body">
                     You are logged in!
